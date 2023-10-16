9c8d15ac163a14fdac50677b43736cf9dfe820a1
EoinFitz03 <G00418325@atu.ie>
2023-10-16 10:28:15 +0100

    adding the subtract , multiply and divide all together

--- src/main/java/ie/atu/calculator.java
+++ src/main/java/ie/atu/calculator.java
@@ -6,8 +6,9 @@ import java.util.Scanner;
 
 public class calculator {
     public static void main(String[]args){
+        multiply();
         divide();
-
+        subtract();
 }
 
 public static void divide()
@@ -24,7 +25,28 @@ public static void divide()
 
 
 }
-public static void delete(){
+public static void multiply(){
+        System.out.println("Please your first number : ");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please your second number : ");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber * secondNumber;
+        System.out.println("The total is " + total);
 
 }
+    public static void subtract(){
+        System.out.println("Please your first number : ");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please your second number : ");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber - secondNumber;
+        System.out.println("The total is " + total);
+
+    }
 }
